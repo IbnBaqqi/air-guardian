ca2d87a84a7a5aba2de95597f816a37a9082e3f4
Abdulbaki <Salausmart132@gmail.com>
2025-07-17 14:36:04 +0300

    local initial

---
+++ src/main/java/com/salausmart/airguardian/AirGuardianApplication.java
@@ -0,0 +1,13 @@
+package com.salausmart.airguardian;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirGuardianApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AirGuardianApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/salausmart/airguardian/AirGuardianApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salausmart.airguardian;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirGuardianApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
